e9403d423e70f87b4381598d23a4112e0a5627ea
Yashkumar Jani <89257321+yashkumarjani@users.noreply.github.com>
2023-07-31 18:15:22 +0530

    Loader Added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import ComingSoon from './Pages/ComingSoon';
+import Loader from './Features/Loader/Loader';
 
 const App = () => {
+  const [loader, setLoader] = useState(true)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoader(false);
+    }, 6000);
+  });
+  
   return (
     <div className="App">
-      <ComingSoon />
+      {
+        loader ? <Loader /> : <ComingSoon />
+      }
     </div>
   )
 }
